3d3226dc076aedc4578d18da2a54e08f4a9af529
Rubxn <rubenbm198@gmail.com>
2023-02-19 14:58:18 +0100

    Validación input

--- 05-react-buscador-peliculas/src/App.jsx
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -1,22 +1,42 @@
 import './App.css'
+import { useRef, useState, useEffect } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
-
+import { useSearch } from './hooks/useSearch'
 
 function App() {
   const {movies} = useMovies()
-  
+  const inputRef = useRef()
+  const {search, setSearch, error} = useSearch()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+   /*  const value = inputRef.current.value
+    console.log(value)
+
+    // con esto podemos recoger todos los campos del formulario
+    // fields.title
+    const fields = Object.fromEntries(new window.FormData(event.target))
+    console.log(fields.title) */
+  }
+
+  function handleChange(event) { 
+    setSearch(event.target.value)
+  }
+
   return (
     <div className='page'>
 
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Harry Potter, Star Wars ...'></input>
+        <form className='form' onSubmit={handleSubmit}>
+          <input style={{border:'1px solid transparent',
+                        borderColor: error ? 'red' : 'transparent'}}
+          onChange={handleChange} value={search} name='title' ref={inputRef} placeholder='Avengers, Harry Potter, Star Wars ...'></input>
           {/* Por defecto, el último botón es el type submit */}
           <button type='submit'>Buscar</button>
         </form>
-
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies}/>
