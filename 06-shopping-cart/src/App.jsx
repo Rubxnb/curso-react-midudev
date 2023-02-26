16443e17d1c5cdbd74fcd62669f7ad5db88352b7
Rubxn <rubenbm198@gmail.com>
2023-02-26 19:08:46 +0100

    Filtros con useContext

--- 06-shopping-cart/src/App.jsx
+++ 06-shopping-cart/src/App.jsx
@@ -2,7 +2,6 @@ import {products as initialProducts} from './mocks/products.json'
 import { Products } from "./components/Products"
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { useState } from 'react'
 import {useFilters} from './hooks/useFilters'
 
 
